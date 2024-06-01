8d54960ba9bb5be053138351c787d0d221e34d27
chetana.lotliker <chetanalotliker@gmail.com>
2024-06-01 12:22:28 +0530

    Create a initial welcome endpoint

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.listen(PORT,
     () => console.log(`It is listening on http://localhost:${PORT}`)
 );
 
+app.get('/', async (req, res) => {
+    res.status(200).send("Welcome to Node Fake Store API project");
+})
+
 app.get('/fake-store', async (req, res) => {
     const response = await axios.get("https://fakestoreapi.com/products");
     res.status(200).send(response.data);
